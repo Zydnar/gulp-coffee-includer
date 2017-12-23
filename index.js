14163d27b6f5086b124008a2c775fab2bbfefa7b
Zydnar <shirkaen@gmail.com>
2017-12-23 19:01:22 +0100

    #2 fixed "gulp-watch works only for first change than crashes"

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const through2 = require('through2'),
     CI = require('coffee-includer'),
     Includer = new CI.Includer();
 
-module.exports = through2.obj(Includer, Includer._transform);
+module.exports = through2.obj(Includer._transform);
